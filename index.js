65f62cca5eea4e5901b5a71b3b93e442ab922cdc
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-03 20:37:20 -0500

    changed some spacing in the readme template

--- index.js
+++ index.js
@@ -4,36 +4,37 @@ const fs = require('fs');
 const generateReadme = (answers) =>
     `# ${answers.title}
     
-     ## Description
-     ${answers.description}
+## Description
+${answers.description}
 
 ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
     
 ## Installation
-    ${answers.instructions}
+${answers.instructions}
 
 ## Usage
-    ${answers.usage}
+${answers.usage}
 
 ## License
-    ![badge](https://img.shields.io/badge/license-${answers.license}-blue))
 
-    This project was created using the ${answers.license} license.
+![badge](https://img.shields.io/badge/license-${answers.license}-blue)
+
+This project was created using the ${answers.license} license.
 
 ## Contributing
-    ${answers.guidelines}
+${answers.guidelines}
 
 ## Tests
-    ${answers.test}
+${answers.test}
 
 ## Questions
-    If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at [${answers.github}](https://github.com/${answers.github}/)`;
+If you have any questions about the repo, open an issue or contact me directly at ${answers.email}. You can find more of my work at [${answers.github}](https://github.com/${answers.github}/)`;
 
 inquirer
     .prompt([
