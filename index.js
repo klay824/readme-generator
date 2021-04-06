0be31dd6f6e5852329aa3a4a184540c8c16931b5
katelyn chadwell <kchadwell0226@gmail.com>
2021-04-06 18:44:00 -0500

    changed wording of some questions and added a validate function to a few questions

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateReadme = (answers) =>
-    `# ${answers.title}
+    `# ${answers.title} ![badge](https://img.shields.io/badge/license-${answers.license}-blue)
+    
     
 ## Description
 ${answers.description}
+
 
 ## Table of Contents
 * [Installation](#installation)
@@ -15,20 +17,22 @@ ${answers.description}
 * [Tests](#tests)
 * [Questions](#questions)
     
+
 ## Installation
 Please run *${answers.instructions}* in your terminal.
+
 
 ## Usage
 ${answers.usage}
 
 ## License
 
-![badge](https://img.shields.io/badge/license-${answers.license}-blue)
+This project is covered under the ${answers.license} license.
 
-This project was created using the ${answers.license} license.
 
 ## Contributing
 ${answers.guidelines}
+
 
 ## Tests
 To run tests, run *${answers.test}* in your terminal.
@@ -43,31 +47,55 @@ inquirer
             type: 'input',
             name: 'title',
             message: 'What is your project title?',
+            validate: function(response) {
+                if(response.length < 1) {
+                    return console.log("Please enter a title for your project to continue.")
+                }
+                return true;
+            }
         },
         {
             type: 'input',
             name: 'description',
             message: 'Please provide a description of your project.',
+            validate: function(response) {
+                if(response.length < 1) {
+                    return console.log("Please enter a description of your project to continue")
+                }
+                return true;
+            }
         },
         {
             type: 'input',
             name: 'instructions',
-            message: 'What are the installation instructions for your project?',
+            message: 'Please provide the terminal command to install your project.',
         },
         {
             type: 'input',
             name: 'usage',
             message: 'How do contributors use your project?',
+            validate: function(response) {
+                if(response.length < 1 ) {
+                    return console.log("Please enter usage instructions to continue.")
+                }
+                return true;
+            }
         },
         {
             type: 'input',
             name: 'guidelines',
             message: 'What contributor guidelines do you have?',
+            validate: function(response) {
+                if(response.length < 1 ) {
+                    return console.log("Please enter contributor guidelines to continue.")
+                }
+                return true;
+            }
         },
         {
             type: 'input',
             name: 'test',
-            message: 'How do users test your project?',
+            message: 'Please enter the terminal command for users to test your project.',
         },
         {
             type: 'list',
@@ -79,16 +107,28 @@ inquirer
             type: 'input',
             name: 'github',
             message: 'What is your GitHub username?',
+            validate: function(response) {
+                if(response.length < 1 ) {
+                    return console.log("Please enter your GitHub username to continue.")
+                }
+                return true;
+            }
         },
         {
             type: 'input',
             name: 'email',
             message: 'What is your email address?',
+            validate: function(response) {
+                if(response.length < 1 ) {
+                    return console.log("Please enter a valid email address to continue.")
+                }
+                return true;
+            }
         },
     ])
     .then((answers) => {
         const readmePageContent = generateReadme(answers);
-        fs.writeFile('generatedREADME.md', readmePageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created generatedREADME.md!')
+        fs.writeFile('./utils/README.md', readmePageContent, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md in your utils folder!')
         );
     });
